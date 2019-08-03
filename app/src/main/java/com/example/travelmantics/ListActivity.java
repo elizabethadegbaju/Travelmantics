548a635e68018417451ea91a283dafe999bf7486
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2019-08-03 09:39:07 +0100

    FireBase UI Auth integration using email and google

--- app/src/main/java/com/example/travelmantics/ListActivity.java
+++ app/src/main/java/com/example/travelmantics/ListActivity.java
@@ -1,14 +1,20 @@
 package com.example.travelmantics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import static com.example.travelmantics.FirebaseUtil.attachListener;
 import static com.example.travelmantics.FirebaseUtil.detachListener;
@@ -29,6 +35,17 @@ public class ListActivity extends AppCompatActivity {
             case R.id.insert_menu:
                 Intent intent = new Intent(this, DealActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("Logout", "onComplete: User logged out");
+                                attachListener();
+                            }
+                        });
+                detachListener();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
