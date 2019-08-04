cd86455b19814edb5f05d0db4c7b674e99671612
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2019-08-04 08:01:05 +0100

    Added User authorization rules and image upload, edit and delete using FireBase Storage. UI Edits and Logo added

--- app/src/main/java/com/example/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -1,14 +1,21 @@
 package com.example.travelmantics;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,16 +24,19 @@ import java.util.List;
 public class FirebaseUtil {
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
+    public static FirebaseStorage mFirebaseStorage;
+    public static StorageReference mStorageReference;
     public static FirebaseUtil firebaseUtil;
     public static FirebaseAuth mFireBaseAuth;
     public static FirebaseAuth.AuthStateListener mAuthListener;
     public static ArrayList<TravelDeal> mDeals;
+    public static boolean isAdmin;
 
     private static final int RC_SIGN_IN = 123;
-    private static Activity caller;
+    private static ListActivity caller;
     private FirebaseUtil(){}
 
-    public static void openFbReference(String ref, final Activity callerActivity){
+    public static void openFbReference(String ref, final ListActivity callerActivity){
         if(firebaseUtil==null){
             firebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = FirebaseDatabase.getInstance();
@@ -38,12 +48,52 @@ public class FirebaseUtil {
                     if (firebaseAuth.getCurrentUser()==null) {
                         FirebaseUtil.signIn();
                     }
-                    Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
+                    else{
+                        String userId = firebaseAuth.getUid();
+                        checkAdmin(userId);
+                        Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
+                    }
                 }
             };
+            connectStorage();
         }
         mDeals = new ArrayList<TravelDeal>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+
+    }
+
+    private static void checkAdmin(String userId) {
+        FirebaseUtil.isAdmin = false;
+        DatabaseReference databaseReference = mFirebaseDatabase.getReference().child("administrators").child(userId);
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                FirebaseUtil.isAdmin = true;
+                Log.d("Admin", "onChildAdded: Admin status verified!");
+                caller.showMenu();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        databaseReference.addChildEventListener(listener);
     }
 
     private static void signIn(){
@@ -56,6 +106,7 @@ public class FirebaseUtil {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setLogo(R.drawable.logo)
                         .build(),
                 RC_SIGN_IN);
     }
@@ -66,5 +117,10 @@ public class FirebaseUtil {
 
     public static void detachListener(){
         mFireBaseAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    public static void connectStorage(){
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mStorageReference = mFirebaseStorage.getReference().child("deals_pictures");
     }
 }
